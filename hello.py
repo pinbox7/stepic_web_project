8a111f11875865a492326edfe2c731246d6a59d7
pinbox7 <pinbox7@gmaile.com>
2021-03-08 15:50:35 +0300

    отредактировал

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def wsgi_application(environ, start_response):
    status = '200 OK'
    headers = [('Content-Type', 'text/plain')]
-   body = [bytes(i + "\r\n" for i in environ['QUERY_STRING'].split('&'))]
+   body = [bytes((i + "\r\n") for i in environ['QUERY_STRING'].split('&'))]
    start_response(status, headers)
    return body
 
