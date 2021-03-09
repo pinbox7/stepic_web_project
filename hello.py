a60ceca47199f76d6ca90eb802e13c349886c19c
pinbox7 <pinbox7@gmaile.com>
2021-03-09 09:57:29 +0300

    v2

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 def wsgi_application(environ, start_response):
    status = '200 OK'
    headers = [('Content-Type', 'text/plain')]
+   print(environ['QUERY_STRING'])
    body = [bytes(i+'\n', 'ascii') for i in environ['QUERY_STRING'][2:].split('&')]
    start_response(status, headers)
    return body
