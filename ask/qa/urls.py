c73585642e561817e3b3a42d6d695e1f1dab5194
pinbox7 <pinbox7@gmaile.com>
2021-03-10 15:50:25 +0300

    django1

---
+++ ask/qa/urls.py
@@ -0,0 +1,11 @@
+from qa.views import test
+
+urlpatterns = [
+    url(r'^$', test, name='test'),
+    url(r'^question/(?P<pk>\d+)/', test, name='test'),
+    url(r'^popular/', test, name='test'),
+    url(r'^ask/', test, name='test'),
+    url(r'^signup/', test, name='test'),
+    url(r'^login/', test, name='test'),
+    url(r'^new/', test, name='test'),
+]
---
+++ ask/qa/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+
+
+def test(request, *args, **kwargs):
+    return HttpResponse('OK')
+
+
