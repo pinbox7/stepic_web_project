0a413c3a337de79bdc5e6d92bdcf9013f85fe563
pinbox7 <pinbox7@gmaile.com>
2021-03-11 16:52:02 +0300

    django2

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -1,11 +1,11 @@
 from qa.views import test
 
 urlpatterns = [
-    url(r'^$', test, name='test'),
-    url(r'^question/(?P<pk>\d+)/', test, name='test'),
-    url(r'^popular/', test, name='test'),
-    url(r'^ask/', test, name='test'),
-    url(r'^signup/', test, name='test'),
-    url(r'^login/', test, name='test'),
-    url(r'^new/', test, name='test'),
+    url(r'^$', test),
+    url(r'^question/(?P<id>[0-9]+)/$', test, name='question'),
+    url(r'^popular/.*', test, name='popular'),
+    url(r'^ask/.*', test, name='ask'),
+    url(r'^signup/.*', test, name='signup'),
+    url(r'^login/.*$', test, name='login'),
+    url(r'^new/.*', test, name='new'),
 ]
