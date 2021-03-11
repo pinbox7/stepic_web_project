ece8762e2213ce9b085ed2f0a7c3867c86098816
pinbox7 <pinbox7@gmaile.com>
2021-03-11 14:10:50 +0300

    django2

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ sudo /etc/init.d/nginx restart
 
 # sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 
 #sudo /etc/init.d/gunicorn restart
