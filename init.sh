1d4592d65717401f94735181113901640928c31b
pinbox7 <pinbox7@gmaile.com>
2021-03-12 09:39:05 +0300

    django2

--- init.sh
+++ init.sh
@@ -1,9 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-# sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_appilication
-# sudo gunicorn -c /home/box/web/etc/gunicorn_ask.conf ask.wsgi:application
-
 # sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
