9753c1bbff7475668689147e56b859111a130ccd
pinbox7 <pinbox7@gmaile.com>
2021-03-17 09:33:00 +0300

    mysql1

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
+# Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+# Gunicorn
 # sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
-
 #sudo /etc/init.d/gunicorn restart
-sudo service mysql restart
+
+# run MySQL & create db
+sudo /etc/init.d/mysql start
+#mysql -uroot -e "create database stepic_web_db"
