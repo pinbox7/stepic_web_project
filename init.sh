d846f6be35525a5ba7f6ad3de4d6aaf2d27a6214
pinbox7 <pinbox7@gmaile.com>
2021-03-17 13:26:13 +0300

    mysql1

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ sudo /etc/init.d/nginx restart
 # sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
 sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
-#sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/gunicorn restart
 
 # run MySQL & create db
 sudo /etc/init.d/mysql restart
