c209f67e986ed1169004662965614219e62efdd8
pinbox7 <pinbox7@gmaile.com>
2021-03-27 10:02:13 +0300

    mysql3

--- init.sh
+++ init.sh
@@ -5,11 +5,15 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # Gunicorn
-# sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask_q
+sudo ln -sf /home/box/web/etc/ask_conf.conf /etc/gunicorn.d/ask_
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 
 # run MySQL & create db
 sudo /etc/init.d/mysql restart
-mysql -uroot -e "create database stepic_web_db"
+mysql -uroot -e "CREATE DATABASE `stepic_web_db` /*!40100 DEFAULT CHARACTER SET utf8*/"
+mysql -uroot -e "CREATE USER 'a_user'@'localhost' IDENTIFIED BY '12345678';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web_db.* TO 'a_user'@'localhost'
+                WITH GRANT OPTION;"
+mysql -uroot -e "FLUSH PRIVILEGES;"
