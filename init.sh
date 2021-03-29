1a5fd39b94be2f1697093f61ff3f4656949da166
pinbox7 <pinbox7@gmaile.com>
2021-03-29 13:25:53 +0300

    mysql3

--- init.sh
+++ init.sh
@@ -12,16 +12,4 @@ sudo /etc/init.d/gunicorn restart
 
 # run MySQL & create db
 sudo /etc/init.d/mysql restart
-cd web/ask
-mysql -uroot -e "CREATE DATABASE `stepic_web_db`
-                DEFAULT CHARACTER SET utf8
-                DEFAULT COLLATE utf8_general_ci;"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON stepic_web_db.* TO 'a_user'@'localhost'
-                WITH GRANT OPTION;"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-sudo python3 manage.py makemigrations qa
-sudo python3 manage.py migrate qa
-
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/gunicorn restart
-
+mysql -uroot -e "create database stepic_web_db"
