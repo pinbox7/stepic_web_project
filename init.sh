b69b0585251535fab97dd22710504b2448b8e00f
pinbox7 <pinbox7@gmaile.com>
2021-03-31 09:20:18 +0300

    mysql3

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # Gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask_q
-sudo ln -sf /home/box/web/etc/ask_conf.conf /etc/gunicorn.d/ask_
+sudo ln -sf /home/box/web/etc/gunicorn.py /etc/gunicorn.d/ask_q
+sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_
 # sudo gunicorn -c /etc/gunicorn.d/ask_conf.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
 
